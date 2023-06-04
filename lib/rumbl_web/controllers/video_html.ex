13dcc46c989a8fa3c259c05d9a6ef476eb027747
Jonny Saykosy <josayko@pm.me>
2023-06-04 17:55:49 +0200

    7.Ecto Queries and Constraints - Diving Deeper into Ecto Queries

--- lib/rumbl_web/controllers/video_html.ex
+++ lib/rumbl_web/controllers/video_html.ex
@@ -1,5 +1,6 @@
 defmodule RumblWeb.VideoHTML do
   use RumblWeb, :html
+  alias Rumbl.Multimedia.Category
 
   embed_templates "video_html/*"
 
@@ -8,6 +9,13 @@ defmodule RumblWeb.VideoHTML do
   """
   attr :changeset, Ecto.Changeset, required: true
   attr :action, :string, required: true
+  attr :categories, Category, required: true
 
   def video_form(assigns)
+
+  def category_select_options(categories) do
+    options = for category <- categories, do: {category.name, category.id}
+    IO.inspect(options, label: "options")
+    options
+  end
 end
